8ad0e68f19cc557fec37f530baf2eac7bf3e1d34
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-09-08 19:44:05 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def readme():
+  with open('README.md', encoding='utf-8') as f:
+    return f.read()
+
+
+setup(
+    name="cimcb",
+    version="2.0.0",
+    description="A package containing the necessary tools for the statistical analysis of untargeted and targeted metabolomics data.",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    url="https://github.com/KevinMMendez/cimcb",
+    packages=["cimcb", "cimcb.bootstrap", "cimcb.cross_val", "cimcb.model", "cimcb.plot", "cimcb.utils"],
+    python_requires=">=3.5",
+    install_requires=["bokeh>=1.0.0",
+                      "keras",
+                      "numpy>=1.12",
+                      "pandas",
+                      "scipy",
+                      "scikit-learn",
+                      "statsmodels",
+                      "tensorflow",
+                      "tqdm",
+                      "xlrd",
+                      "joblib"],
+    author="Kevin Mendez, David Broadhurst",
+    author_email="k.mendez@ecu.edu.au, d.broadhurst@ecu.edu.au",
+)
