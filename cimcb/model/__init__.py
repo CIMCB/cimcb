5733718694960dae15c68a1aed2ff32c1196643e
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-11-07 14:49:09 +0800

    Add files via upload

--- cimcb/model/__init__.py
+++ cimcb/model/__init__.py
@@ -1,6 +1,5 @@
 from .NN_LinearSigmoid import NN_LinearSigmoid
 from .NN_SigmoidSigmoid import NN_SigmoidSigmoid
-from .NN_SoftmaxSoftmax import NN_SoftmaxSoftmax
 from .MBNN_LinearSigmoid import MBNN_LinearSigmoid
 from .MBNN_SigmoidSigmoid import MBNN_SigmoidSigmoid
 from .MBNN_LinearSigmoid_1Layer import MBNN_LinearSigmoid_1Layer
@@ -16,27 +15,7 @@ from .RF import RF
 from .SVM import SVM
 from .NN_L1 import NN_L1
 from .NN_L2 import NN_L2
-from .NN_LinearLinear import NN_LinearLinear
-from .NN_LinearLogit import NN_LinearLogit
-from .NN_LinearTan import NN_LinearTan
-from .NN_LinearRelu import NN_LinearRelu
-from .NN_LogitLinear import NN_LogitLinear
-from .NN_LogitLogit import NN_LogitLogit
-from .NN_LogitTan import NN_LogitTan
-from .NN_LogitRelu import NN_LogitRelu
-from .NN_TanLinear import NN_TanLinear
-from .NN_TanLogit import NN_TanLogit
-from .NN_TanTan import NN_TanTan
-from .NN_TanRelu import NN_TanRelu
-from .NN_ReluLinear import NN_ReluLinear
-from .NN_ReluLogit import NN_ReluLogit
-from .NN_ReluTan import NN_ReluTan
-from .NN_ReluRelu import NN_ReluRelu
 from .RBF_NN import RBF_NN
 
 
-__all__ = ["NN_LinearSigmoid", "NN_SigmoidSigmoid", "NN_SoftmaxSoftmax", "MBNN_LinearSigmoid", "MBNN_SigmoidSigmoid", "NN_SigmoidSigmoidSigmoid", "NN_LinearLogit_Sklearn", "NN_LogitLogit_Sklearn", "PCLR", "PCR", "PLS_SIMPLS", "PLS_NIPALS", "RF", "SVM", "NN_L1", "NN_L2",
-           "NN_LinearLinear", "NN_LinearLogit", "NN_LinearTan", "NN_LinearRelu", " NN_LogitLinear", "NN_LogitLogit",
-           "NN_LogitTan", "NN_LogitRelu",
-           "NN_TanLinear", "NN_TanLogit", "NN_TanTan", "NN_TanRelu",
-           "NN_ReluLinear", "NN_ReluLogit", "NN_ReluTan", "NN_ReluRelu", "RBF_NN"]
+__all__ = ["NN_LinearSigmoid", "NN_SigmoidSigmoid", "NN_SoftmaxSoftmax", "MBNN_LinearSigmoid", "MBNN_SigmoidSigmoid", "NN_SigmoidSigmoidSigmoid", "NN_LinearLogit_Sklearn", "NN_LogitLogit_Sklearn", "PCLR", "PCR", "PLS_SIMPLS", "PLS_NIPALS", "RF", "SVM", "NN_L1", "NN_L2","RBF_NN"]
