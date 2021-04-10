f6775d51b9b3b3f3fca32463f7d6cb78a67ccd8d
Kevin Milton Mendez <35716119+KevinMMendez@users.noreply.github.com>
2021-04-10 18:09:06 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="cimcb",
-    version="2.0.42",
+    version="2.1.1",
     description="A package containing the necessary tools for the statistical analysis of untargeted and targeted metabolomics data.",
     long_description=readme(),
     long_description_content_type='text/markdown',
